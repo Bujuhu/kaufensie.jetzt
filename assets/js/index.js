de5225d28720ef1a77d14bba16ec4b4245b4641a
Juri Schreib <juri@schreib.at>
2015-05-14 11:52:24 +0200

    Removed unnecessary javascript

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-var items = document.getElementsByClassName("item");
-
-for(var i=0; i<items.length; i++) {
-	items[i].addEventListener('click', function(){
-		window.open(this.getElementsByTagName("a")[0].textContent,"_self");
-	});
-}
